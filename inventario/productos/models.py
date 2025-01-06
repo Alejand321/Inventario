9bc1158f2c240c4f34b0fbf1b19895f1e7b1b0cc
alejandrovelloso <alejandro.velloso@viridis.com.py>
2025-01-06 17:30:49 -0300

    cambios html y css

--- inventario/productos/models.py
+++ inventario/productos/models.py
@@ -3,11 +3,11 @@ from django.db import models
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.CharField(max_length=300)
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    precio = models.IntegerField()
     disponible = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"{self.nombre}"
+        return self.nombre
 
 class Caracteristica(models.Model):
     producto = models.ForeignKey(Producto, related_name='caracteristicas', on_delete=models.CASCADE)
